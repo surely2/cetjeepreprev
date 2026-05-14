718894aa5d8b145c76336fb8e66963220daa2321
surely2 <84121137+surely2@users.noreply.github.com>
2026-05-14 19:06:26 +0530

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -1,5 +1,5 @@
 export { SUBJECTS } from './subjects.jsx';
-export { PHYSICS_DATA } from './physicsData.js';
+export { PHYSICS_DATA } from './physicsData.jss';
 export { CHEMISTRY_DATA } from './chemistryData.js';
 export { MATHEMATICS_DATA } from './mathData.jsx';
 export { 
