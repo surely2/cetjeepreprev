2b98daf73e6497600d7a9ec08d877401d958f38a
surely2 <84121137+surely2@users.noreply.github.com>
2026-05-14 19:27:18 +0530

    Delete src directory

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { Math, useMathReady } from "./Math.jsx";
-export { TagBadge, WeightageBadge, FormulaCard } from "./FormulaCard.jsx";
-export { SectionBlock } from "./SectionBlock.jsx";
-export { Sidebar } from "./Sidebar.jsx";
-export { MobileBottomNav, MobileDrawer, MainNavDrawer } from "./MobileNav.jsx";
